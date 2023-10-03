76493340bee5dc66ffb22a62d980fc1bf8de5bbf
pawan kumar <pawan@pawans-MacBook-Pro.local>
2023-10-03 21:46:45 +0530

    added a new method getEmail

--- account.js
+++ account.js
@@ -1,13 +1,17 @@
 class Account {
     constructor(){
         this.abc = "hellow";
+        this.email = "itmanpawan@gmail.com"
     }
 
     getAccountDetails(){
         return this.abc;
     }
 
-    
+    getEmail(){
+        return this.email;
+    }
+
 }
 
 
